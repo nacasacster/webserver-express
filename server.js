d1b3da9def82a9450a05f8ecbcd25d221154d762
mwonline <nathalycastro@mwonline.com.co>
2018-07-25 16:53:07 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+//heroku entrega de manera global el puerto 
+const port = process.env.port || 3000;
+
+//midelware
+app.use(express.static(__dirname + '/public'));
+//Expredd HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'fernando',
+    });
+});
+app.get('/about', (req, res) => {
+    res.render('about', {
+        //nombre: 'fernando',
+    });
+});
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
